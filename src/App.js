0a0a3e6173d0b6067ea78efa6a909f1e62641706
brendanxure <lindaamadigwe@hotmail.com>
2022-08-25 23:08:03 +0200

    Change Readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [username, setUsername] = useState('')
+    const [validUsername, setValidusername] = useState(false)
+
+    const [password, setPassword] = useState('')
+    const [validPassword, SetValidPassword] = useState(false)
+
+
+    return ( 
+        <div className = "App" >
+            <form onSubmit={(e)=> e.preventDefault()}>
+                <h2>Welcome, Please register</h2>
+                <label htmlFor="username">
+                    Username:
+                </label>
+                <input id="username" type='text' />
+                <label htmlFor="password">
+                    Password:
+                </label>
+                <input id="password" type='password'  />
+                <label htmlFor="confirm" >
+                    Confirm Password:
+                </label>
+                <input id="confirm" type='password' />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
